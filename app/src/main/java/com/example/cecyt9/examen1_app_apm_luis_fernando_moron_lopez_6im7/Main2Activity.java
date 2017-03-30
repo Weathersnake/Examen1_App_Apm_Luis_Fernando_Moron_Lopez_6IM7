e5173633bb4a53fa4f6dc7a31c1898c391319e9d
CECyT9 <CECyT9>
2017-03-30 08:26:33 -0600

    @emmanuel789

---
+++ app/src/main/java/com/example/cecyt9/examen1_app_apm_luis_fernando_moron_lopez_6im7/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.cecyt9.examen1_app_apm_luis_fernando_moron_lopez_6im7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/example/cecyt9/examen1_app_apm_luis_fernando_moron_lopez_6im7/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cecyt9.examen1_app_apm_luis_fernando_moron_lopez_6im7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickConvertir(){
+        EditText editText=(EditText)
+    }
+}
